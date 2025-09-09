2660751f8eedab740eb6fed71d7e66e434e1ee4a
Jonathan214777 <jonarojasp@udistrital.edu.co>
2025-09-09 17:17:49 -0500

    Update CalculadoraJava.java

--- Corte /PracticasIniciales/CalculadoraJava.java
+++ Corte /PracticasIniciales/CalculadoraJava.java
@@ -1,34 +1,35 @@
-package Calculator;
-public class calculator {
-    public calculator(){
-        
- }
+package calculator;
 
-public int add(int a, int b){
-    return a+b;
- }
+public class Calculator{
+    public Calculator(){
+}
 
-public int substract(int a, int b){
-    return a-b;
- }
+public int add(int a, int b ) {
+    return a + b;
 
-public int multiply(int a, int b){
+}
+
+public int substract (int a, int b){
+    return a - b ;
+}
+
+public int multiply(int a , int b){
     return a*b;
- }
+}
 
 public double divide(int a, int b){
-    return a/b;
- }
+    return a/b;  
+}
 
 public int modulo(int a, int b){
     return a%b;
- }
 
-public static void main(string[]args){
-    calculator mycalculator = new calculator();
-    system.out.println(mycalculator.add(5, 7));
-    system.out.println(mycalculator.substract(45, 11));
-    system.out.println(mycalculator.multiply(3, 2));
- }
+}
 
+public static void main(String[] args) {
+    Calculator MYCALCULATOR = new Calculator();
+    System.out.println(MYCALCULATOR.add(5,7));
+    System.out.println(MYCALCULATOR.substract(45,11));
+    System.out.println(MYCALCULATOR.multiply(3,2));
+}
 }
