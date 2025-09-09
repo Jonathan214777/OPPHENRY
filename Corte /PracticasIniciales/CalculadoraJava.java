927e04a1e0a37bd72a9cae14235362d5a16009c4
Jonathan214777 <jonarojasp@udistrital.edu.co>
2025-09-09 17:34:47 -0500

    Update CalculadoraJava.java

--- Corte /PracticasIniciales/CalculadoraJava.java
+++ Corte /PracticasIniciales/CalculadoraJava.java
@@ -1,35 +1,44 @@
 package calculator;
+import java.util.Scanner;
 
-public class Calculator{
-    public Calculator(){
-}
+public class Calculator {
+    public Calculator() {
+    }
 
-public int add(int a, int b ) {
-    return a + b;
+    public int add(int a, int b) {
+        return a + b;
+    }
 
-}
+    public int substract(int a, int b) {
+        return a - b;
+    }
 
-public int substract (int a, int b){
-    return a - b ;
-}
+    public int multiply(int a, int b) {
+        return a * b;
+    }
 
-public int multiply(int a , int b){
-    return a*b;
-}
+    public double divide(int a, int b) {
+        return a / b;
+    }
 
-public double divide(int a, int b){
-    return a/b;  
-}
+    public int modulo(int a, int b) {
+        return a % b;
+    }
 
-public int modulo(int a, int b){
-    return a%b;
+    public static void main(String[] args) {
+        Scanner digito = new Scanner(System.in);
 
-}
+        System.out.print("Escriba el primer dígito: ");
+        int D1 = digito.nextInt();
 
-public static void main(String[] args) {
-    Calculator MYCALCULATOR = new Calculator();
-    System.out.println(MYCALCULATOR.add(5,7));
-    System.out.println(MYCALCULATOR.substract(45,11));
-    System.out.println(MYCALCULATOR.multiply(3,2));
-}
+        System.out.print("Escriba el segundo dígito: ");
+        int D2 = digito.nextInt();
+
+        Calculator myCalculator = new Calculator();
+        System.out.println("Suma: " + myCalculator.add(D1, D2));
+        System.out.println("Resta: " + myCalculator.substract(D1, D2));
+        System.out.println("Multiplicación: " + myCalculator.multiply(D1, D2));
+        System.out.println("División: " + myCalculator.divide(D1, D2));
+        System.out.println("Módulo: " + myCalculator.modulo(D1, D2));
+    }
 }
